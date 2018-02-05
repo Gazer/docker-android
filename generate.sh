9889764d4aa58f822bfdda5a6e24cb978c8c87df
Ricardo Markiewicz <ricardo.markiewicz@patagonian.it>
2018-02-05 20:09:14 -0300

    Added Tools version 26.1.1

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 
 SDK_VERSION=`grep sdk_version versions | cut -d "=" -f 2`
 
-for version in `seq 19 25` ; do
+for version in `seq 19 26` ; do
   BUILD_TOOLS=`grep tools$version versions | cut -d "=" -f 2`
 
   echo "Creating Dockerfile$version sdk $SDK_VERSION tools $BUILD_TOOLS"
